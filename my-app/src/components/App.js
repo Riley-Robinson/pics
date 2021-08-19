112c12c38ec964b494c242b9a51f443fc3cf5ee9
Riley Robinson <rrtoinfinity@gmail.com>
2021-08-19 11:47:13 -0400

    Update App.js

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    state ={ images: [] };
+
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos?', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 3yeG-oW4bsGnP09SKKSLWeHokdPZMqHa_qPCEEYhMWo'
+            }
+        });
+
+        this.setState({ images: response.data.results });    
     }
 
     render() {
             return (
                 <div className="ui container" style={{ marginTop: '10px'}}>
                         <SearchBar onSubmit={this.onSearchSubmit} />
+                        found: {this.state.images.length} images
+                
                 </div>
             );
         }

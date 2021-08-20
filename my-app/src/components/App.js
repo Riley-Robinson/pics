443378241dbcac6ff747d001e1d13576137604f6
Riley Robinson <rrtoinfinity@gmail.com>
2021-08-20 13:45:37 -0400

    adding axios
    adding axios to app

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 
@@ -8,12 +8,10 @@ class App extends React.Component {
 
     state ={ images: [] };
 
-    async onSearchSubmit(term) {
-        const response = await axios.get('https://api.unsplash.com/search/photos?', {
-            params: { query: term },
-            headers: {
-                Authorization: 'Client-ID 3yeG-oW4bsGnP09SKKSLWeHokdPZMqHa_qPCEEYhMWo'
-            }
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos?', {
+            params: { query: term }
+            
         });
 
         this.setState({ images: response.data.results });    
